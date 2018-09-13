a555ac783a75008f29e570055994b0a3e8fa1169
Alan Davis Babu <alanb@qburst.com>
2018-09-13 20:45:43 +0530

    Craeted HTML CSS JS files for currentstock module

--- js/index.js
+++ js/index.js
@@ -1,6 +1,20 @@
 function content(url) {
     req = new XMLHttpRequest();
-    req.open("GET",url, false);
+    req.open("GET", url, false);
     req.send(null);
     document.getElementById("content").innerHTML = req.responseText;
+}
+window.onload = function (event) {
+    if (!localStorage.getItem("stock")) {
+        var data = fetch('../current-stock/json/current-stock.json')
+            .then(res => res.json())
+            .then((out) => {
+                var stock = JSON.stringify(out);
+                localStorage.setItem("stock", stock);
+            }).catch(err => console.error(err));
     }
+    if (!localStorage.getItem("inbound")) {
+        localStorage.setItem("inbound", "[]");
+
+    }
+}
